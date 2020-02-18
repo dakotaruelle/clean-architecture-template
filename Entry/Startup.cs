79557ab39b0912d9a7afb7384a1196cc1d9a3c9e
Dakota Ruelle <druelle@omnitech-inc.com>
2020-02-18 16:31:28 -0600

    Create a Web project to contain Controllers and Views

--- Entry/Startup.cs
+++ Entry/Startup.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Web;
+using Web.Controllers;
 
 namespace Entry
 {
@@ -21,11 +16,6 @@ namespace Entry
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
